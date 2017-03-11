e8981143bcb5d7f3d82aa93028f35e3cedc6b675
tbcooney <taylor.cooney@vegastudios.ca>
2017-03-11 16:53:03 -0500

    Add job views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,14 +1,25 @@
 class JobsController < ApplicationController
+	before_action :find_job, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@jobs = Job.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@job = Job.new
 	end
 
 	def create
+		@job = Job.new(job_params)
+
+		if @job.save
+			redirect_to @job
+		else
+			render 'New'
+		end
 	end
 
 	def update
@@ -20,8 +31,10 @@ class JobsController < ApplicationController
 	private
 
 	def find_job
+		@job = Job.find(params[:id])
 	end
 
 	def job_params
+		params.require(:job).permit(:title, :description, :company, :url)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources	:jobs
-
-
 end
