3ea8f0b74d3eae0a127bf00361ab8550e357773c
tbcooney <taylor.cooney@vegastudios.ca>
2017-03-11 15:47:52 -0500

    Generate jobs controller and add routes

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,27 @@
+class JobsController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_job
+	end
+
+	def job_params
+	end
+end
