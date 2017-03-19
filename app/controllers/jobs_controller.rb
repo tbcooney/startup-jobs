13da7a7e9166b01cd3347bf6770e363f8b2dbadd
tbcooney <taylor.cooney@vegastudios.ca>
2017-03-18 21:53:13 -0400

    Modified job index to group.by job.created_at, removed @job_days from jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,11 +5,9 @@ class JobsController < ApplicationController
 	def index
 		if params[:category].blank?
 			@jobs = Job.where(Job.arel_table[:created_at].gteq("%D")).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
-			@job_days = @jobs.group_by {|job| job.created_at.to_date }
 		else
 			@category_id = Category.find_by(name: params[:category]).id
 			@jobs = Job.where(category_id: @category_id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
-			@job_days = @jobs.group_by {|job| job.created_at.to_date }
 		end
 	end
 
